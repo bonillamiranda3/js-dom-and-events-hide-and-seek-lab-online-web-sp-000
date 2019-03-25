e5211e990d4a36331fbc4ece455b0965ee7cfea9
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-25 01:15:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+   
+}
+
+funcion increaseRankBy(n) {
+  let rank = document.querySelectorAll('.ranked-list');
+    for (let i = 0; i < rank.length; i++) {
+      rank[i].innerHTML = (i + 1).toString();
+    }
+}
+
+function deepestChild() {
+  let deepest = document.querySelector('#grand-node').querySelectorAll('div')
+  return deepest [deepest.length - 1];
+
+}
+
